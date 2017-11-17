8278c5cde6bfdb2264d4ccf93fdd2c92513822e4
emmalaroudie <laroudie.e@gmail.com>
2017-11-17 15:29:04 +0100

    ajout partie graphique (view)

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,16 +1,26 @@
 package controller;
 
 import view.*;
+
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 import model.*;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		CommPort commPort = new CommPort();
-		Serial serial = new Serial();
-		serial.serialLauncher(commPort.commPortFinder());
 		
-		DisplayLoop loop = new DisplayLoop(serial);
-		loop.Loop();
+		Fenetre fenetrePrincipale = new Fenetre();
+		//Code Maxime Lavergne@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+		//		CommPort commPort = new CommPort();
+		//		Serial serial = new Serial();
+		//		serial.serialLauncher(commPort.commPortFinder());
+		//		DisplayLoop loop = new DisplayLoop(serial);
+		//		loop.Loop();
+		//Code Maxime Lavergne@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 	}
 }
--- src/view/Panel.java
+++ src/view/Panel.java
@@ -1,26 +1,26 @@
-package view;
-
-import java.awt.Graphics;
-import javax.swing.JPanel;
-
-import model.Serial;
-
-public class Panel extends JPanel {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	Serial serial;
-	public Panel(Serial serial) {
-		this.serial = serial;
-	}
-
-	public void paintComponent(Graphics g) {
-		g.drawString(serial.getChunks()[0], 10, 20);
-		System.out.println(serial.getChunks()[0]);
-	}
-	
-	public void update() {
-		this.repaint();
-	}
-}
+//package view;
+//
+//import java.awt.Graphics;
+//import javax.swing.JPanel;
+//
+//import model.Serial;
+//
+//public class Panel extends JPanel {
+//	/**
+//	 * 
+//	 */
+//	private static final long serialVersionUID = 1L;
+//	Serial serial;
+//	public Panel(Serial serial) {
+//		this.serial = serial;
+//	}
+//
+//	public void paintComponent(Graphics g) {
+//		g.drawString(serial.getChunks()[0], 10, 20);
+//		System.out.println(serial.getChunks()[0]);
+//	}
+//	
+//	public void update() {
+//		this.repaint();
+//	}
+//}
